6ec3acae6d76c5112177875766edb60efa7cc726
Stéphane LAINÉ <stephanelaine@Stephanes-MacBook-Air.local>
2024-08-28 15:40:18 -0400

    Mise à jour du dossier DocumentconceptionProjetfinalTDS2023S avec les dernières modifications

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,18 @@ import { Box } from '@mui/material';
 import Exercises from '../components/Exercises';
 import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+import { useAuth } from '../../contexts/authContext'
 
 const Home = () => {
+  const { currentUser } = useAuth();
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
 
   return (
     <Box>
+      <div className='text-2xl font-bold pt-14'>
+        Hello {currentUser.displayName ? currentUser.displayName : currentUser.email}, you are now logged in.
+      </div>
       <HeroBanner />
       <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
       <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
